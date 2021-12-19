70d216719f3271d46bdee363728e1374d2cdde87
SudoAzek <45741494+SudoAzek@users.noreply.github.com>
2021-12-19 21:12:18 +0100

    Create P03_Move_Element_To_End.py

---
+++ Medium/P03_Move_Element_To_End.py
@@ -0,0 +1,18 @@
+def moveElementToEnd(array, toMove):
+    i = 0
+    j = len(array) - 1
+    while i < j:
+        # [1, 3, 2, 4, 2, 2, 2]
+        swap = 0
+        while i < j and array[j] == toMove:
+            j -= 1
+        if array[i] == toMove:
+            array[i], array[j] = array[j], array[i]
+            # swap = array[i]
+            # array[i] = array[j]
+            # array[j] = swap
+        i += 1
+    return array
+
+
+print(moveElementToEnd([1, 3, 2, 4, 2, 2, 2], 2))
